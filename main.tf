f402a0d83ce92c4ec100af95be8ad8f4eca5ae43
Aleksandr Vinogradov <iiojib.vi@gmail.com>
2020-07-29 16:43:23 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   is_t_instance_type = replace(var.instance_type, "/^t(2|3|3a){1}\\..*$/", "1") == "1" ? true : false
 }
 
-resource "aws_instance" "this" {
+[resource/ "aws_instance" "this" {
   count = var.instance_count
 
   ami              = var.ami
